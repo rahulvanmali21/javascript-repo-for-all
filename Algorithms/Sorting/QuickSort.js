9f99bd5fe8cd7c6ba92d0edfcb48381fd5077c1b
rahul vanmali <vanmalirahul1996@gmail.com>
2022-10-06 23:21:43 +0530

    quick sort added

---
+++ Algorithms/Sorting/QuickSort.js
@@ -0,0 +1,20 @@
+const quickSort = function (array) {
+  if (array.length < 2) {
+    return array;
+  }
+  let left = [];
+  let right = [];
+  let pivot = array.at(-1);
+  for (let index = 0; index < array.length - 1; index++) {
+    const element = array[index];
+    if (element > pivot) {
+      right.push(element);
+    } else {
+      left.push(element);
+    }
+  }
+  return [...quickSort(left), pivot, ...quickSort(right)];
+};
+
+let a = [10, 2, 33, 4, 5];
+console.log(quickSort(a));
